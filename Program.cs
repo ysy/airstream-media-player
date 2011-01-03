7f063118eaca00ff2d683600d9b3e0e4ff874b2a
code@tomthorpe.co.uk <code@tomthorpe.co.uk@0d03175f-7565-ef8f-9c92-1ae2cd87759a>
2011-01-03 17:44:57 +0000

    Initial commit
    git-svn-id: http://airstream-media-player.googlecode.com/svn/trunk@8 0d03175f-7565-ef8f-9c92-1ae2cd87759a

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AirStreamPlayer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Publish());
+        }
+    }
+}
