ecaf3471ec635455ec9461c1c8d49dc86c3beb3f
code@tomthorpe.co.uk <code@tomthorpe.co.uk@0d03175f-7565-ef8f-9c92-1ae2cd87759a>
2011-01-03 17:44:45 +0000

    Initial commit
    git-svn-id: http://airstream-media-player.googlecode.com/svn/trunk@5 0d03175f-7565-ef8f-9c92-1ae2cd87759a

---
+++ CheckboxMessageBox.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace AirStreamPlayer
+{
+    partial class CheckboxMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.message = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dontshowagain = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // message
+            // 
+            this.message.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.message.Location = new System.Drawing.Point(12, 24);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(360, 43);
+            this.message.TabIndex = 0;
+            this.message.Text = "label1";
+            this.message.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(296, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dontshowagain
+            // 
+            this.dontshowagain.AutoSize = true;
+            this.dontshowagain.Location = new System.Drawing.Point(15, 81);
+            this.dontshowagain.Name = "dontshowagain";
+            this.dontshowagain.Size = new System.Drawing.Size(172, 17);
+            this.dontshowagain.TabIndex = 2;
+            this.dontshowagain.Text = "Don\'t show this message again";
+            this.dontshowagain.UseVisualStyleBackColor = true;
+            // 
+            // CheckboxMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 112);
+            this.Controls.Add(this.dontshowagain);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.message);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "CheckboxMessageBox";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox dontshowagain;
+    }
+}
