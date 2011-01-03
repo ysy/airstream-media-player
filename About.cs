cd3b7e1e9ec9f2df82ef48b60f74369f39c09c58
code@tomthorpe.co.uk <code@tomthorpe.co.uk@0d03175f-7565-ef8f-9c92-1ae2cd87759a>
2011-01-03 17:45:38 +0000

    Initial commit
    git-svn-id: http://airstream-media-player.googlecode.com/svn/trunk@16 0d03175f-7565-ef8f-9c92-1ae2cd87759a

---
+++ About.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace AirStreamPlayer
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Launches default browser to go to www.tomthorpe.com
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bylink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.tomthorpe.com");
+        }
+    }
+}
